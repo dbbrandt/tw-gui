cbfa632db71c05717417ef16f7814842f8722f9f
Daniel Brandt <dbbrandt@gmail.com>
2025-11-04 18:33:39 -0500

    backup: preserve LLK-based state and local menu changes before switching to TurboWarp base

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -18,7 +18,7 @@ import Divider from '../divider/divider.jsx';
 import SaveStatus from './save-status.jsx';
 import ProjectWatcher from '../../containers/project-watcher.jsx';
 import MenuBarMenu from './menu-bar-menu.jsx';
-import {MenuItem, MenuSection} from '../menu/menu.jsx';
+import {MenuItem, MenuSection, Submenu} from '../menu/menu.jsx';
 import ProjectTitleInput from './project-title-input.jsx';
 import AuthorInfo from './author-info.jsx';
 import AccountNav from '../../containers/account-nav.jsx';
@@ -75,6 +75,7 @@ import {
 import collectMetadata from '../../lib/collect-metadata';
 
 import styles from './menu-bar.css';
+import settingsMenuStyles from './settings-menu.css';
 
 import helpIcon from '../../lib/assets/icon--tutorials.svg';
 import mystuffIcon from './icon--mystuff.png';
@@ -517,6 +518,32 @@ class MenuBar extends React.Component {
                                                 />
                                             </MenuItem>
                                         )}</SB3Downloader>
+                                    </MenuSection>
+                                    <MenuSection>
+                                        <MenuItem>
+                                            <div className={settingsMenuStyles.option}>
+                                                <span className={settingsMenuStyles.submenuLabel}>
+                                                    <FormattedMessage
+                                                        defaultMessage="Lesson Toolbox"
+                                                        description="Lesson toolbox submenu"
+                                                        id="gui.menuBar.lessonToolbox"
+                                                    />
+                                                </span>
+                                                <img
+                                                    className={settingsMenuStyles.expandCaret}
+                                                    src={dropdownCaret}
+                                                />
+                                            </div>
+                                            <Submenu place={this.props.isRtl ? 'left' : 'right'}>
+                                                <MenuItem>
+                                                    <FormattedMessage
+                                                        defaultMessage="Coming soon"
+                                                        description="Placeholder item for Lesson Toolbox submenu"
+                                                        id="gui.menuBar.lessonToolbox.comingSoon"
+                                                    />
+                                                </MenuItem>
+                                            </Submenu>
+                                        </MenuItem>
                                     </MenuSection>
                                 </MenuBarMenu>
                             </div>
